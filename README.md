b688d8152fd474c9ed80ccff2922597a1a091c1d
Will Weaver <monkey@buildingbananas.com>
2019-03-31 23:44:40 -0400

    Sync Master Vault to Decks Of Keyforge

---
+++ README.md
@@ -0,0 +1,3 @@
+# KeyforgeSync
+
+A script to sync keyforge decks from the Master Vault to other community services.
---
+++ app.js
@@ -0,0 +1,89 @@
+require("dotenv").config();
+const request = require("request-promise-native");
+
+const MasterVaultToken = process.env.MV_TOKEN;
+const MasterVaultUserId = process.env.MV_USER;
+const DoKBearerToken = process.env.DOK_TOKEN;
+
+const getMyDecksFromMV = (page = 1, prev = []) => {
+  return request({
+    method: "GET",
+    url: `https://www.keyforgegame.com/api/users/${MasterVaultUserId}/decks/`,
+    qs: {
+      page: page,
+      page_size: "30",
+      ordering: "-date"
+    },
+    headers: { authorization: MasterVaultToken },
+    json: true
+  }).then(res => {
+    const decks = prev.concat(
+      res.data.map(deck => ({
+        name: deck.name,
+        id: deck.id
+      }))
+    );
+
+    // Return all results if complete
+    if (decks.length >= res.count) return decks;
+    else return getMyDecksFromMV(page + 1, decks);
+  });
+};
+
+const getMyDecksFromDoK = (page = 0, prev = []) => {
+  return request({
+    method: "POST",
+    url: "https://decksofkeyforge.com/api/decks/filter",
+    headers: {
+      authorization:
+        "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ3aWxsaWFtZHdlYXZlckBnbWFpbC5jb20iLCJpYXQiOjE1NTQwNjkyNjAsImV4cCI6MTU1NDY3NDA2MCwiUm9sZSI6IlVTRVIifQ.vk4mkrxlxpDLWtQUmr4Kp8J1lrb7Ssl9PlCX6f7-r73AetJz3ohpQrTZzo9qCzvGxUSamAM_J6S1ReBPJbqjoQ",
+      "content-type": "application/json"
+    },
+    body: {
+      page: page,
+      sortDirection: "DESC",
+      owner: "funkymonkeymonk"
+    },
+    json: true
+  }).then(res => {
+    // Return all results if complete
+    if (res.decks.length === 0) return prev;
+
+    const decks = prev.concat(
+      res.decks.map(deck => ({
+        name: deck.name,
+        id: deck.keyforgeId
+      }))
+    );
+
+    return getMyDecksFromDoK(page + 1, decks);
+  });
+};
+
+const importDecksIntoDoK = decks => {
+  if (decks.length === 0) console.log("No new decks to import.");
+  for (let deck of decks) {
+    console.log(`Importing ${deck.name} into DoK`);
+    request({
+      method: "POST",
+      url: `https://decksofkeyforge.com/api/decks/${deck.id}/import-and-add`,
+      headers: { authorization: DoKBearerToken }
+    })
+      .then(console.log(`Imported ${deck.name} into DoK`))
+      .catch(err => console.log(`Import failed`));
+  }
+};
+
+const delta = (source, destination) => {
+  console.log(
+    `${source.length} decks in source -> ${
+      destination.length
+    } decks in destination`
+  );
+  return source.filter(deck => !destination.map(d => d.id).includes(deck.id));
+};
+
+Promise.all([getMyDecksFromMV(), getMyDecksFromDoK()])
+  .then(([mv, kof]) => delta(mv, kof))
+  .then(importDecksIntoDoK)
+  .catch(err => console.log(err.message));
