6d184bf6185586f91ec51887b5c44821806d138f
Will Weaver <monkey@buildingbananas.com>
2019-04-01 00:53:41 -0400

    Sync Master Vault to Crucible

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const request = require("request-promise-native");
 const MasterVaultToken = process.env.MV_TOKEN;
 const MasterVaultUserId = process.env.MV_USER;
 const DoKBearerToken = process.env.DOK_TOKEN;
+const CrucibleToken = process.env.CRUCIBLE_TOKEN;
+const CrucibleUserId = process.env.CRUCIBLE_USER;
 
 const getMyDecksFromMV = (page = 1, prev = []) => {
   return request({
@@ -74,6 +76,43 @@ const importDecksIntoDoK = decks => {
   }
 };
 
+const getMyDecksFromCrucible = () => {
+  return request({
+    method: "GET",
+    url: "https://www.thecrucible.online/api/decks",
+    qs: { _: CrucibleUserId },
+    headers: {
+      authorization: CrucibleToken
+    },
+    json: true
+  }).then(res => {
+    return res.decks.map(deck => ({
+      name: deck.identity,
+      id: deck.uuid
+    }));
+  });
+};
+
+const importDecksIntoCrucible = decks => {
+  if (decks.length === 0) console.log("No new decks to import.");
+
+  for (let deck of decks) {
+    console.log(`Importing ${deck.name} into Crucible`);
+    request({
+      method: "POST",
+      url: "https://www.thecrucible.online/api/decks/",
+      headers: {
+        authorization: CrucibleToken,
+        "content-type": "application/json"
+      },
+      body: { uuid: deck.id },
+      json: true
+    })
+      .then(console.log(`Imported ${deck.name} into Crucible`))
+      .catch(err => console.log(`Import failed`));
+  }
+};
+
 const delta = (source, destination) => {
   console.log(
     `${source.length} decks in source -> ${
@@ -86,4 +125,9 @@ const delta = (source, destination) => {
 Promise.all([getMyDecksFromMV(), getMyDecksFromDoK()])
   .then(([mv, kof]) => delta(mv, kof))
   .then(importDecksIntoDoK)
+  .catch(err => console.log(err.message));
+
+Promise.all([getMyDecksFromMV(), getMyDecksFromCrucible()])
+  .then(([mv, crucible]) => delta(mv, crucible))
+  .then(importDecksIntoCrucible)
   .catch(err => console.log(err.message));
