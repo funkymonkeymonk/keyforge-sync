a37290b5f5a62c10ccd88634abb7c6883c11493f
Will Weaver <monkey@buildingbananas.com>
2019-04-03 17:15:30 -0400

    Switch to login from bearer token for crucible

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ MasterVault.getMyDecks()
   .then(mvDecks => {
     if (dok) {
       const DoK = require("./dok");
-      const dokEmail = process.env.DOK_EMAIL;
-      const dokPassword = process.env.DOK_PASSWORD;
-      const dokUserName = process.env.DOK_USER_NAME;
+      const email = process.env.DOK_EMAIL;
+      const password = process.env.DOK_PASSWORD;
+      const username = process.env.DOK_USERNAME;
 
-      DoK.login(dokEmail, dokPassword)
+      DoK.login(email, password)
         .then(token => {
-          DoK.getMyDecks(dokUserName, token)
+          DoK.getMyDecks(username, token)
             .then(dokDecks => delta(mvDecks, dokDecks))
             .then(decks => DoK.importDecks(token, decks))
             .catch(err => {
-              console.log("Error connecting to Decks Of Keyforge");
+              console.log("Error syncing Decks Of Keyforge");
               console.log(err.message);
             });
         })
@@ -37,12 +37,22 @@ MasterVault.getMyDecks()
           console.log(err.message);
         });
     }
+
     if (crucible) {
       const Crucible = require("./crucible");
+      const username = process.env.CRUCIBLE_USERNAME;
+      const password = process.env.CRUCIBLE_PASSWORD;
 
-      Crucible.getMyDecks()
-        .then(crucibleDecks => delta(mvDecks, crucibleDecks))
-        .then(Crucible.importDecks)
+      Crucible.login(username, password)
+        .then(token => {
+          Crucible.getMyDecks(token)
+            .then(crucibleDecks => delta(mvDecks, crucibleDecks))
+            .then(decks => Crucible.importDecks(token, decks))
+            .catch(err => {
+              console.log("Error syncing Crucible");
+              console.log(err.message);
+            });
+        })
         .catch(err => {
           console.log("Error connecting to Crucible");
           console.log(err.message);
--- crucible.js
+++ crucible.js
@@ -1,15 +1,14 @@
 const request = require("request-promise-native");
 
-const CrucibleToken = process.env.CRUCIBLE_TOKEN;
 const CrucibleUserId = process.env.CRUCIBLE_USER;
 
-const getMyDecks = () => {
+const getMyDecks = token => {
   return request({
     method: "GET",
     url: "https://www.thecrucible.online/api/decks",
     qs: { _: CrucibleUserId },
     headers: {
-      authorization: CrucibleToken
+      authorization: "Bearer " + token
     },
     json: true
   }).then(res => {
@@ -20,7 +19,7 @@ const getMyDecks = () => {
   });
 };
 
-const importDecks = decks => {
+const importDecks = (token, decks) => {
   if (decks.length === 0) console.log("No new decks to import.");
 
   for (let deck of decks) {
@@ -29,7 +28,7 @@ const importDecks = decks => {
       method: "POST",
       url: "https://www.thecrucible.online/api/decks/",
       headers: {
-        authorization: CrucibleToken,
+        authorization: "Bearer " + token,
         "content-type": "application/json"
       },
       body: { uuid: deck.id },
@@ -40,4 +39,16 @@ const importDecks = decks => {
   }
 };
 
-module.exports = { getMyDecks, importDecks };
+const login = (username, password) => {
+  return request({
+    method: "POST",
+    url: "https://www.thecrucible.online/api/account/login",
+    body: {
+      username: username,
+      password: password
+    },
+    json: true
+  }).then(res => res.token);
+};
+
+module.exports = { getMyDecks, importDecks, login };
