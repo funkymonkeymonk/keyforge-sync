11c9b78d31891f204d86a404a5b84b9d6155e395
Will Weaver <monkey@buildingbananas.com>
2020-12-27 12:58:49 -0500

    Fix testing setup so that it runs

--- test/commands/sync.test.ts
+++ test/commands/sync.test.ts
@@ -1,10 +1,10 @@
-// import { expect, test } from "@oclif/test";
-//
-// xdescribe("sync", () => {
-//   test
-//     .stdout()
-//     .command(["sync"])
-//     .it("runs hello", ctx => {
-//       expect(ctx.stdout).to.contain("hello world");
-//     });
-// });
+import { expect, test } from "@oclif/test";
+
+xdescribe("sync", () => {
+  test
+    .stdout()
+    .command(["sync"])
+    .it("runs hello", ctx => {
+      expect(ctx.stdout).to.contain("hello world");
+    });
+});
